eb70a43706e8ae867e96ecb2c61da6d133dc4782
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-07-25 15:57:15 +0300

    Homework. Format.

--- app.js
+++ app.js
@@ -1,83 +1,91 @@
-const startBtn = document.querySelector("#start")
-const screens = document.querySelectorAll(".screen")
-const timeList = document.querySelector("#time-list")
-const timeEl = document.querySelector("#time")
-const board = document.querySelector("#board")
-const colors = ["#a4c2f4", "#9c98eb", "#de98eb", "#ea6c89", "#6cea9f", "#bdd96a", "#d9ac6a"]
+const startBtn = document.querySelector("#start");
+const screens = document.querySelectorAll(".screen");
+const timeList = document.querySelector("#time-list");
+const timeEl = document.querySelector("#time");
+const board = document.querySelector("#board");
+const colors = [
+  "#a4c2f4",
+  "#9c98eb",
+  "#de98eb",
+  "#ea6c89",
+  "#6cea9f",
+  "#bdd96a",
+  "#d9ac6a",
+];
 
-let time = 0
-let score = 0
+let time = 0;
+let score = 0;
 
 const startGame = () => {
-    setInterval(decreaseTime, 1000)
-    createRandomCircle()
-    setTime(time)
-}
+  setInterval(decreaseTime, 1000);
+  createRandomCircle();
+  setTime(time);
+};
 
 const setTime = (value) => {
-    timeEl.innerHTML = `00:${value}`
-}
+  timeEl.innerHTML = `00:${value}`;
+};
 
 const decreaseTime = () => {
-    if (time === 0) {
-        finishGame()
-    } else {
-        let current = --time
-        if (current < 10) {
-            current = `0${current}`
-        }
-        setTime(current)
+  if (time === 0) {
+    finishGame();
+  } else {
+    let current = --time;
+    if (current < 10) {
+      current = `0${current}`;
     }
-}
+    setTime(current);
+  }
+};
 
 const finishGame = () => {
-    timeEl.parentNode.classList.add('hide')
-    board.innerHTML = `<h1>Счёт: <span class="primary">${score}</span></h1>`
-}
+  timeEl.parentNode.classList.add("hide");
+  board.innerHTML = `<h1>Счёт: <span class="primary">${score}</span></h1>`;
+};
 
 const createRandomCircle = () => {
-    const circle = document.createElement('div')
-    const size = getRandomNumber(10, 60)
-    const {width, height} = board.getBoundingClientRect()
-    const x = getRandomNumber(0, width - size)
-    const y = getRandomNumber(0, height - size)
-    const color = getRandomColor();
+  const circle = document.createElement("div");
+  const size = getRandomNumber(10, 60);
+  const { width, height } = board.getBoundingClientRect();
+  const x = getRandomNumber(0, width - size);
+  const y = getRandomNumber(0, height - size);
+  const color = getRandomColor();
 
-    circle.classList.add('circle')
-    circle.style.width = `${size}px`
-    circle.style.height = `${size}px`
-    circle.style.top = `${y}px`
-    circle.style.left = `${x}px`
-    circle.style.background = `${color}`
+  circle.classList.add("circle");
+  circle.style.width = `${size}px`;
+  circle.style.height = `${size}px`;
+  circle.style.top = `${y}px`;
+  circle.style.left = `${x}px`;
+  circle.style.background = `${color}`;
 
-    board.append(circle)
-}
+  board.append(circle);
+};
 
 const getRandomNumber = (min, max) => {
-    return Math.round(Math.random() * (max-min) + min)
-}
+  return Math.round(Math.random() * (max - min) + min);
+};
 
 const getRandomColor = () => {
-    return colors[Math.floor(Math.random() * colors.length)];
-  };
+  return colors[Math.floor(Math.random() * colors.length)];
+};
 
 startBtn.addEventListener("click", (event) => {
-    event.preventDefault()
-    screens[0].classList.add('up')
-})
+  event.preventDefault();
+  screens[0].classList.add("up");
+});
 
 timeList.addEventListener("click", (event) => {
-    if(event.target.classList.contains("time-btn")) {
-        time = parseInt(event.target.getAttribute('data-time'))
-        screens[1].classList.add('up')
-        startGame()
-    }
-})
+  if (event.target.classList.contains("time-btn")) {
+    time = parseInt(event.target.getAttribute("data-time"));
+    screens[1].classList.add("up");
+    startGame();
+  }
+});
 
 board.addEventListener("click", (event) => {
-    if (event.target.classList.contains('circle')) {
-        score++
-        event.target.remove()
-        createRandomCircle()
-    }
-})
+  if (event.target.classList.contains("circle")) {
+    score++;
+    event.target.remove();
+    createRandomCircle();
+  }
+});
